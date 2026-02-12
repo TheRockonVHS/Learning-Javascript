e4c1c10b6b4681d4ffb451a2efb9803395d41264
Kurt Busche <kbusche01@gmail.com>
2026-02-12 07:48:01 -0500

    started Day 3

--- Day 3.js
+++ Day 3.js
@@ -1,1 +1,21 @@
-```javascript // your code here ```
+```javascript // code here ```
+
+// Day 2 DOM Basics
+const myButton = document.querySelector('.myBtn');
+
+myButton.addEventListener('click', () => {
+  console.log('Clicked!');
+})
+
+// Day 2 DOM Basics v2
+const myPara = document.querySelector('.output');
+
+myPara.textContent('Goodbye');
+
+// Day 1 Functions
+function calculateTotal(price, taxRate) {
+  return price + (price * taxRate);
+}
+
+// a parameter is inside the () from the function and arguments are values passed into the function when you call the function
+
