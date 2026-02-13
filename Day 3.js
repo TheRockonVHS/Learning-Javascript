3b6873821299ca06f6d6598ac011f1044efd7587
Kurt Busche <kbusche01@gmail.com>
2026-02-13 08:06:00 -0500

    finished Mini-app 1 from Day 3

--- Day 3.js
+++ Day 3.js
@@ -1,4 +1,4 @@
-```javascript // code here ```
+// ```javascript // code here ```
 
 // Day 2 DOM Basics
 const myButton = document.querySelector('.myBtn');
@@ -19,3 +19,21 @@ function calculateTotal(price, taxRate) {
 
 // a parameter is inside the () from the function and arguments are values passed into the function when you call the function
 
+// Mini-App 1: Button Counter
+const countDisplay = document.querySelector('#count');
+const button = document.querySelector('#counterBtn');
+let count = 0;
+
+button.addEventListener('click', () => {
+  count++;
+  countDisplay.textContent = "Count: " + count;
+})
+
+// Mini-App 2: Live Text Display
+const textInput = document.querySelector("#textInput");
+const paragraph = document.querySelector("#display");
+
+textInput.addEventListener("keydown", () => {
+  // stuff here
+  console.log('works')
+})
