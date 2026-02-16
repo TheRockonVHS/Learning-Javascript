d34a63f9210d4cf205ccdabfa16470abfe012f22
Kurt Busche <kbusche01@gmail.com>
2026-02-16 07:32:34 -0500

    completed Reboot Day 3

--- Reboot Day 3.js
+++ Reboot Day 3.js
@@ -7,9 +7,8 @@ for (let index = 0; index < fruits.length; index++) {
   console.log(output);
 }
 
-fruits.forEach(element => {
-  let output = fruits[element];
-  console.log(output);
+fruits.forEach(item => {
+  console.log(item);
 });
 
 // Exercise 3
@@ -19,3 +18,30 @@ console.log(fruits);
 // Exercise 4
 fruits.pop();
 console.log(fruits);
+
+// Exercise 5
+// // 1. Select the parent container
+// const list = document.querySelector('#fruitList');
+
+// // 2. Loop through fruits
+// fruits.forEach(fruit => {
+//   // 3. Create a NEW element (not on list, on document)
+//   const li = document.createElement('li');
+  
+//   // 4. Add content to it
+//   li.textContent = fruit;
+  
+//   // 5. Append it to the parent
+//   list.appendChild(li);  // or list.append(li)
+// });
+
+``` javascript
+const list = document.querySelector('#fruitList');
+
+for (let index = 0; index < fruits.length; index++) {
+  const output = fruits[index];
+  const li = document.createElement('li');
+  li.textContent = output;
+  list.append(li);  
+}
+```
