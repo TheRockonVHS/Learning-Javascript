79215e9e1d3b79418cd2f6c2aabf98c4fb91f2f8
Kurt Busche <kbusche01@gmail.com>
2026-02-15 22:41:32 -0500

    finished Day 3 Mini-App 2

--- Day 3.js
+++ Day 3.js
@@ -1,39 +1,39 @@
 // ```javascript // code here ```
 
-// Day 2 DOM Basics
-const myButton = document.querySelector('.myBtn');
+// // Day 2 DOM Basics
+// const myButton = document.querySelector('.myBtn');
 
-myButton.addEventListener('click', () => {
-  console.log('Clicked!');
-})
+// myButton.addEventListener('click', () => {
+//   console.log('Clicked!');
+// })
 
-// Day 2 DOM Basics v2
-const myPara = document.querySelector('.output');
+// // Day 2 DOM Basics v2
+// const myPara = document.querySelector('.output');
 
-myPara.textContent('Goodbye');
+// myPara.textContent('Goodbye');
 
-// Day 1 Functions
-function calculateTotal(price, taxRate) {
-  return price + (price * taxRate);
-}
+// // Day 1 Functions
+// function calculateTotal(price, taxRate) {
+//   return price + (price * taxRate);
+// }
 
-// a parameter is inside the () from the function and arguments are values passed into the function when you call the function
+// // a parameter is inside the () from the function and arguments are values passed into the function when you call the function
 
-// Mini-App 1: Button Counter
-const countDisplay = document.querySelector('#count');
-const button = document.querySelector('#counterBtn');
-let count = 0;
 
-button.addEventListener('click', () => {
-  count++;
-  countDisplay.textContent = "Count: " + count;
-})
+// // Mini-App 1: Button Counter
+// const countDisplay = document.querySelector('#count');
+// const button = document.querySelector('#counterBtn');
+// let count = 0;
+
+// button.addEventListener('click', () => {
+//   count++;
+//   countDisplay.textContent = "Count: " + count;
+// })
 
 // Mini-App 2: Live Text Display
 const textInput = document.querySelector("#textInput");
 const paragraph = document.querySelector("#display");
 
-textInput.addEventListener("keydown", () => {
-  // stuff here
-  console.log('works')
+textInput.addEventListener("input", () => {
+  paragraph.textContent = "You typed: " + textInput.value;
 })
