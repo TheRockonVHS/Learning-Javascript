7dce8593c78585eb486381895b3651da8c4450c7
Kurt Busche <kbusche01@gmail.com>
2026-03-02 22:59:30 -0500

    mastery check

---
+++ Reboot Day 7 v2.js
@@ -0,0 +1,16 @@
+// Write code that does this:
+
+// -   There's a button with the id `toggleBtn`
+// -   There's a paragraph with the id `message`
+// -   When the button is clicked, the paragraph's text changes to `"Hello!"`
+
+// That's it. Just write it from memory and paste what you get here — right or wrong.
+
+const button = document.querySelector('#toggleBtn');
+const para = document.querySelector('#message');
+
+button.addEventListener('click', () => {
+  if (!para(hidden)) {
+    para = para(hidden);
+  }
+})
