522e872bb546218e6849a73600ba657082cbbcc8
Kurt Busche <kbusche01@gmail.com>
2026-03-04 23:11:26 -0500

    getting inputs

--- Reboot Day 7 Shopping List.js
+++ Reboot Day 7 Shopping List.js
@@ -1,3 +1,10 @@
 // Build from memory. You may look up syntax for specific methods, but not copy-paste solutions.
 // Task: Build a mini shopping list. The user types an item into an `<input>`, clicks an "Add" button, and the item appears as a new `<li>` in a `<ul>`. No frameworks — vanilla JS only.
 
+const itemInput = document.querySelector('#item-input');
+const button = document.querySelector('#add-btn');
+const list = document.querySelector('shopping-list');
+
+button.addEventListener('click', () => {
+  // code here
+})
