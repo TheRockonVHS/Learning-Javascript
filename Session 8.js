f4ca8c778bcc4b60da5b1846aeb674e46e13dbee
Kurt Busche <kbusche01@gmail.com>
2026-03-10 00:06:43 -0400

    intial commit

---
+++ Session 8.js
@@ -0,0 +1,24 @@
+// 1. On page load, get todos from storage and render them
+function loadTodos() {
+  // TODO: get 'todos' from localStorage
+  // TODO: parse it into an array
+  // TODO: loop through array and render each one
+}
+
+// 2. When user clicks "Add", add to array AND storage AND DOM
+function addTodo(text) {
+  // TODO: create a new todo object
+  // TODO: push it into the array
+  // TODO: save array to localStorage
+  // TODO: render the new todo to the DOM
+}
+
+// 3. Render a single todo to the DOM
+function renderTodo(todo) {
+  // TODO: create an <li> element
+  // TODO: set its text content
+  // TODO: append it to the list
+}
+
+// Call loadTodos when page loads
+loadTodos();
