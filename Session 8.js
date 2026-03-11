2e716603556efcb2b2c81b2ae2f13aca828f20cf
Kurt Busche <kbusche01@gmail.com>
2026-03-11 08:04:21 -0400

    addTodo and renderTodo functions

--- Session 8.js
+++ Session 8.js
@@ -1,5 +1,6 @@
 // ``` javascript
 const list = document.querySelector('#todosList');
+const array = [];
 
 // 1. On page load, get todos from storage and render them
 function loadTodos() {
@@ -9,10 +10,9 @@ function loadTodos() {
     return;
   }
   // TODO: parse it into an array
-  let parsedData = [];
-  parsedData = JSON.parse(todos);  
+  array = JSON.parse(todos);  
   // TODO: loop through array and render each one
-  parsedData.forEach(element => {
+  array.forEach(element => {
     renderTodo(element);
   });
 }
@@ -20,16 +20,25 @@ function loadTodos() {
 // 2. When user clicks "Add", add to array AND storage AND DOM
 function addTodo(text) {
   // TODO: create a new todo object
+  const newObj = {
+    text: text,
+  }
   // TODO: push it into the array
+  newObj.push(array);
   // TODO: save array to localStorage
+  localStorage.setItem('todo', array);
   // TODO: render the new todo to the DOM
+  renderTodo(array);
 }
 
 // 3. Render a single todo to the DOM
 function renderTodo(todo) {
   // TODO: create an <li> element
+  const newItem = document.createElement('li');
   // TODO: set its text content
+  newItem.textContent = todo.text;
   // TODO: append it to the list
+  list.appendChild(newItem);
 }
 
 // Call loadTodos when page loads
