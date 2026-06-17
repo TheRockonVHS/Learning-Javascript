685223dfeb79a74eb965cb78005d11dd18747daf
Kurt Busche <kbusche01@gmail.com>
2026-06-17 08:01:03 -0400

    Session 12 start

--- chat.js
+++ chat.js
@@ -18,7 +18,19 @@ function convertToNumbers() {}
 function calculateCOGS() {}
 function renderPrice() {}
 
-// ```javascript
 if (Number.isNaN(value) || value <= 0 ) {
   return;
 }
+
+// ```javascript
+// 1
+arry = arry.filter(item);
+
+// 2
+// Answer: need to clear the DOM and the render the updated array
+
+// 3
+// prevents the default event from running which is submitting the form
+
+// 4
+// need to make that the input is Number so I can properly compare numbers. A string to number type doesn't compare right.
